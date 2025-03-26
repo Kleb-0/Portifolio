34001d17b41840fa5aa6243f4459934de299b061
Calebe <shadowhilles@gmail.com>
2025-03-26 17:42:01 -0300

    feat: add Skills page with animated skill cards and new assets

---
+++ src/app/skills/page.js
@@ -0,0 +1,47 @@
+"use client";
+
+import Skill from "@/components/skill";
+import { motion } from "framer-motion";
+
+export default function Skills() {
+  return (
+    <div className="bg-zinc-900 min-h-screen p-5">
+      <motion.h1
+        initial={{ opacity: 0, y: 35 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 2.8}}
+        className="text-zinc-100 text-4xl text-center my-10"
+      >
+        My Technical skills
+      </motion.h1>
+
+      <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+        <Skill skill="HTML" skillImg="html.png" />
+        <Skill skill="CSS" skillImg="css.png" />
+        <Skill skill="Javascript" skillImg="javascript.png" />
+        <Skill skill="Tailwind CSS" skillImg="tailwind-css.png" />
+        <Skill skill="Bootstrap" skillImg="bootstrap.png" />
+        <Skill skill="Next" skillImg="next.png" />
+        <Skill skill="React" skillImg="react.png" />
+        <Skill skill="Git" skillImg="git.png" />
+        <Skill skill="Vercel" skillImg="vercel.svg" />
+      </div>
+      <motion.h1
+        initial={{ opacity: 0, y: 35 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 2.8}}
+        className="text-zinc-100 text-4xl text-center my-10"
+      >
+        Studying
+      </motion.h1>
+      <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+        <Skill skill="Java" skillImg="java.png" />
+        <Skill skill="Spring" skillImg="spring.svg" />
+        <Skill skill="PostgreSQL" skillImg="postgresql.png" />
+        <Skill skill="Angular" skillImg="angular.png" />
+        <Skill skill="Docker" skillImg="docker.png" />
+        <Skill skill="Heroku" skillImg="heroku.svg" />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/skill.jsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+export default function Skill({ skill, skillImg }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: -250 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6 }}
+      className="bg-zinc-700 flex flex-col items-center justify-between p-4 h-[120px] rounded-lg shadow-lg hover:scale-110 hover:shadow-violet-600"
+    >
+      <motion.div
+        initial={{ opacity: 0, x: -500 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1.1 }}
+      >
+        <Image
+          className="inline-block"
+          width={50}
+          height={50}
+          src={`/skills/${skillImg}`}
+          alt="Skill"
+        />
+      </motion.div>
+      <motion.p
+        initial={{ opacity: 0, y: 70 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1.8 }}
+        className="text-zinc-100 text-xl mt-auto"
+      >
+        {skill}
+      </motion.p>
+    </motion.div>
+  );
+}
