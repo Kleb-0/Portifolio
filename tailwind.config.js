cf3df77bcb026c20872925b18644ebd75c9d17c4
Kleb-0 <shadowhilles@gmail.com>
2025-07-04 21:58:18 -0300

    feat: add Ringift font and update Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-export const content = [
-  './src/app/**/*.{js,ts,jsx,tsx}',
-  './src/components/**/*.{js,ts,jsx,tsx}',
+module.exports = [
+  "./src/app/**/*.{js,ts,jsx,tsx}",
+  "./src/components/**/*.{js,ts,jsx,tsx}",
 ];
-export const theme = {
+module.exports = {
   extend: {
     colors: {
-      'dark-prmy': '#EAEAEA',
-      'dark-scnd': '#9B4DFF',
-      'dark-bg': '#0D0D0D',
-      'light-prmy': '#0D0D0D',
-      'light-scnd': '#2563EB',
-      'light-bg': '#EAEAEA',
+      "dark-prmy": "#EAEAEA",
+      "dark-scnd": "#9B4DFF",
+      "dark-bg": "#0D0D0D",
+      "light-prmy": "#0D0D0D",
+      "light-scnd": "#2563EB",
+      "light-bg": "#EAEAEA",
+    },
+    fontFamily: {
+      ringift: ["Ringift", "serif"],
     },
   },
 };
-export const plugins = [
-  require('tailwindcss-debug-screens'),
-];
+export const plugins = [require("tailwindcss-debug-screens")];
