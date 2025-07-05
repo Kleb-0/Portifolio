4cd99813f570d494f0a258f96fc1166de4664155
Kleb-0 <shadowhilles@gmail.com>
2025-07-04 22:00:31 -0300

    feat: update NavBar component to include current page state and improve link handling

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,34 +1,47 @@
 "use client";
 
 import Link from "next/link";
+import { useState } from "react";
 
 export default function NavBar() {
+  const [currentPage, setCurrentPage] = useState([0]);
   const glowEffect =
-    "hover:text-violet-600 hover:scale-110 hover:shadow-violet-500 hover:shadow-sm rounded-sm !p-1";
+    "hover:text-violet-600 hover:scale-110 hover:shadow-violet-500 hover:shadow-sm rounded-sm p-1";
   const navItems = ["Home", "Projects", "About", "Skills"];
-  
 
   return (
     <div className="h-20 text-zinc-100 bg-zinc-800 flex items-center">
-      <nav className="lg:text-xl md:text-lg text-lg w-full flex items-center justify-center ">
-        <ul className="flex gap-12 ">
+      <nav className="md:text-lg text-md w-full flex items-center justify-center ">
+        <ul className="flex gap-5 sm:gap-12 ">
           <li>
-            <Link className={glowEffect} href="/">
+            <Link onClick={setCurrentPage[0]} className={glowEffect} href="/">
               {navItems[0]}
             </Link>
           </li>
           <li>
-            <Link className={glowEffect} href="/projects">
+            <Link
+              onClick={setCurrentPage[1]}
+              className={glowEffect}
+              href="/projects"
+            >
               {navItems[1]}
             </Link>
           </li>
           <li>
-            <Link className={glowEffect} href="/about">
+            <Link
+              onClick={setCurrentPage[2]}
+              className={glowEffect}
+              href="/about"
+            >
               {navItems[2]}
             </Link>
           </li>
           <li>
-            <Link className={glowEffect} href="/skills">
+            <Link
+              onClick={setCurrentPage[3]}
+              className={glowEffect}
+              href="/skills"
+            >
               {navItems[3]}
             </Link>
           </li>
