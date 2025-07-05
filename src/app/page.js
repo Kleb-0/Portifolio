c44683761abd6d3180dc3916e37d7c655d0c0475
Kleb-0 <shadowhilles@gmail.com>
2025-07-04 22:00:18 -0300

    feat: enhance Home component layout and update text styles

--- src/app/page.js
+++ src/app/page.js
@@ -6,14 +6,14 @@ import { easeOut, motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen !pl-20 bg-zinc-900 text-zinc-100 justify-center">
+    <div className="flex flex-col min-h-screen items-center bg-zinc-900 text-zinc-100 justify-center md:items-start md:pl-20">
       <motion.p
         initial={{ opacity: 0, x: -500 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.8, ease: easeOut }}
-        className="text-5xl w-fit"
+        className="md:text-5xl sm:text-4xl text-2xl text-center"
       >
-        <strong className="bg-gradient-to-r from-violet-500 to-violet-300 bg-clip-text text-transparent">
+        <strong className="bg-gradient-to-r from-violet-500 to-violet-400 bg-clip-text text-transparent fontFamily-Ringifit">
           Hello,
         </strong>{" "}
         I am
@@ -22,7 +22,7 @@ export default function Home() {
         initial={{ opacity: 0, x: -500 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 1.2, ease: easeOut }}
-        className="text-6xl w-fit"
+        className="md:text-6xl sm:text-5xl text-4xl text-center"
       >
         Calebe Hillesheim Lamb
       </motion.h1>
@@ -31,9 +31,9 @@ export default function Home() {
         initial={{ opacity: 0, x: -500}}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 1.6, ease: easeOut }}
-        className="text-4xl w-fit bg-gradient-to-r from-violet-500 to-violet-300 bg-clip-text text-transparent"
+        className="md:text-4xl sm:text-3xl text-xl text-center bg-gradient-to-r from-violet-500 to-violet-300 bg-clip-text text-transparent"
       >
-        Frontend Developer
+        Software developer
       </motion.h3>
       <div className="flex gap-3 mt-6">
         <motion.a
@@ -44,7 +44,7 @@ export default function Home() {
           href="https://github.com/Kleb-0"
         >
           <Image
-            className="rounded-md hover:scale-110 hover:shadow-violet-500 hover:shadow-md  transition-transform duration-350"
+            className="rounded-md hover:scale-110 hover:shadow-violet-500 hover:shadow-md transition-transform duration-350 max-w-[35px] sm:max-w-full"
             src="/github-logo.png"
             alt="github"
             width={50}
@@ -59,7 +59,7 @@ export default function Home() {
           href="https://www.linkedin.com/in/calebe-hillesheim-lamb-5a86792a5/"
         >
           <Image
-            className="rounded-md  hover:scale-110 hover:shadow-violet-500 hover:shadow-md  transition-transform duration-350"
+            className="rounded-md  hover:scale-110 hover:shadow-violet-500 hover:shadow-md  transition-transform duration-350 max-w-[35px] sm:max-w-full"
             src="/linkedin-logo.png"
             alt="github"
             width={50}
