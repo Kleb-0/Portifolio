0cb5d209c2638bfa41b50b700de6d411ee22fda8
Calebe Hillesheim Lamb <shadowhilles@gmail.com>
2025-10-26 00:44:34 -0300

    feat: enhance layout with custom font and improved metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,24 @@
-import NavBar from "@/components/navbar";
-import Head from "next/head";
-import "@/styles/globals.css"
-import "@/styles/reset.css"
+import './globals.css'
+import { Source_Serif_4 } from 'next/font/google'
+import NavBar from '@/components/NavBar'
+
+const sourceSerif = Source_Serif_4({
+  subsets: ['latin'],
+  variable: '--font-source-serif',
+})
 
 export const metadata = {
-  title: "Calebe",
-};
+  title: 'Calebe H. Lamb',
+  description: 'Feito por Calebe',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="min-h-screen overflow-y-auto">
+      <body className={`${sourceSerif.variable} font-source-serif min-h-screen overflow-y-auto`}>
         <NavBar />
         <main className="flex-grow">{children}</main>
       </body>
     </html>
-  );
+  )
 }
