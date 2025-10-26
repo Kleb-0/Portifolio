2493af195546f627b535d028ccc07638c3335723
Calebe Hillesheim Lamb <shadowhilles@gmail.com>
2025-10-26 01:08:45 -0300

    refactor: clean up layout.js formatting and remove unused utils.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,24 +1,26 @@
-import './globals.css'
-import { Source_Serif_4 } from 'next/font/google'
-import NavBar from '@/components/NavBar'
+import NavBar from "../components/NavBar";
+import "./globals.css";
+import { Source_Serif_4 } from "next/font/google";
 
 const sourceSerif = Source_Serif_4({
-  subsets: ['latin'],
-  variable: '--font-source-serif',
-})
+  subsets: ["latin"],
+  variable: "--font-source-serif",
+});
 
 export const metadata = {
-  title: 'Calebe H. Lamb',
-  description: 'Feito por Calebe',
-}
+  title: "Calebe H. Lamb",
+  description: "Feito por Calebe",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${sourceSerif.variable} font-source-serif min-h-screen overflow-y-auto`}>
+      <body
+        className={`${sourceSerif.variable} font-source-serif min-h-screen overflow-y-auto`}
+      >
         <NavBar />
         <main className="flex-grow">{children}</main>
       </body>
     </html>
-  )
+  );
 }
